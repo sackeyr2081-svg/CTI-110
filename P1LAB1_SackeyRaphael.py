640d10acadfe27e819b280652ce3969c78074cca
sackeyr2081-svg <sackeyr2081@student.faytechcc.edu>
2025-11-05 18:22:54 -0500

    Add files via upload

---
+++ P1LAB1_SackeyRaphael.py
@@ -0,0 +1,12 @@
+# Raphael Sackey
+# November 5, 2025
+# P1LAB1: Welcome Message
+# This program asks the user for their first and last name, 
+# then displays a welcome message to the course.
+
+# Ask for user's first and last name
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Display welcome message
+print("\nHello,", first_name, last_name + "!", "Welcome to CTI-110.")
